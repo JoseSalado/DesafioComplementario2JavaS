cb5c22cec79043f72162f89507f3965c0e5cbea7
JoseSalado <saladojosemiguel@gmail.com>
2022-09-05 23:27:34 -0300

    Termine desafio complementario

--- app.js
+++ app.js
@@ -13,22 +13,36 @@ function comprarProducto(){
     producto = prompt ("Ingrese una Opcion : \n 1: Sorrentinos de Jamon y Queso $500 \n 2: Sorrentinos de Ternera y Queso $550 \n 3: Sorrentinos de Calabaza y Queso $450")
     if(producto === "1"){
         let valorBuscado = "jamon y queso";
-        let resultado = sorrentinos.find(sorrentino => sorrentino.sabor === valorBuscado);
-        alert(resultado);
-    
+        resultado = sorrentinos[0].precio;
+        alert(`eligio : ${valorBuscado} , precio : $ ${resultado}`);
     }
     if(producto === "2"){
         let valorBuscado = "ternera y queso";
-        let resultado = sorrentinos.find(sorrentino => sorrentino.sabor === valorBuscado);
-        alert(resultado);
+        resultado = sorrentinos[1].precio;
+        alert(`eligio : ${valorBuscado} , precio : $ ${resultado}`);
     }
     if(producto === "3"){
         let valorBuscado = "calabaza y queso";
-        let resultado = sorrentinos.find(sorrentino => sorrentino.sabor === valorBuscado);
-        alert(resultado);
+        resultado = sorrentinos[2].precio;
+        alert(`eligio : ${valorBuscado} , precio : $ ${resultado}`);
     }
     
+    opcion = prompt("Que desea realizar   \n 2 : Pagar Compra \n 3 : Salir ");
+    
 }
+
+function finalizarCompra(){
+    if(producto==="1"){
+        alert(`Compro : ${sorrentinos[0].sabor} , total a pagar : $ ${resultado + 100} , incluye gastos de envio`);
+    }
+    else if ( producto==="2"){
+        alert(`Compro : ${sorrentinos[1].sabor} , total a pagar : $ ${resultado + 100} , incluye gastos de envio`);
+    }
+    else if (producto==="3"){
+        alert(`Compro : ${sorrentinos[2].sabor} , total a pagar : $ ${resultado + 100} , incluye gastos de envio`);
+    }
+}
+
 
 
 
